278e86f9fbadcc3da4f0a4905e2f7264c12d5acd
Luiz <lg.luizh@gmail.com>
2019-11-06 10:48:35 -0300

    Primeiro select

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,12 @@ const knexfile = require('../knexfile')
 const knex = require('knex')(knexfile)
 module.exports = knex
 
-knex('users').insert({
-  name: 'Luiz',
-  email: 'lg.luizh@gmail.com',
-  password: 'secret'
-}).then(data => console.log(data))
+// knex('users').insert({
+//   name: 'Luiz',
+//   email: 'lg.luizh@gmail.com',
+//   password: 'secret'
+// }).then(data => console.log(data))
+
+knex('users')
+  .select('*')
+  .then(result => console.log(result))
